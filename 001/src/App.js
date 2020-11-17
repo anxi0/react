7b5563744e3efa865d6e45c947b3d985540dfb6a
anxi0 <jjw01040@gmail.com>
2020-11-17 17:32:19 +0900

    upload 001

---
+++ 001/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Hello from "./components/Hello";
+import "./App.css";
+
+function App() {
+  const name = "react";
+  const style = {
+    backgroundColor: "black",
+    color: "aqua",
+    fontSize: 24,
+    padding: "1rem",
+  };
+  return (
+    <div>
+      <Hello />
+      <div style={style}>{name}</div>
+      <div className="gray-box"></div>
+    </div>
+  );
+}
+
+export default App;
