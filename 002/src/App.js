f5e6eb0fdcff4848db900b2a32cec53e54addc00
anxi0 <jjw01040@gmail.com>
2020-11-17 18:26:49 +0900

    upload 002

---
+++ 002/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Hello from "./Hello";
+import Wrapper from "./Wrapper";
+
+function App() {
+  return (
+    <Wrapper>
+      <Hello name="" color="red" />
+    </Wrapper>
+  );
+}
+
+export default App;
