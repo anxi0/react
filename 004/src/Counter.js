812f78c2a2a6c25935999e40e26b55adf713e9ba
anxi0 <jjw01040@gmail.com>
2020-11-18 09:21:49 +0900

    modified 004

--- 004/src/Counter.js
+++ 004/src/Counter.js
@@ -5,9 +5,18 @@ const Counter = () => {
   return (
     <div>
       <h1>{Number} </h1>
+      <input
+        type="number"
+        pattern="\d*"
+        placeholder="input number to change"
+        value={parseInt(Number) == 0 ? "" : +Number}
+        onChange={event => {
+          setNumber(event.target.value === "" ? 0 : event.target.value);
+        }}
+      />
       <div>
-        <button onClick={() => setNumber(Number + 1)}>+1</button>
-        <button onClick={() => setNumber(Number - 1)}>-1</button>
+        <button onClick={() => setNumber(+Number + 1)}>+1</button>
+        <button onClick={() => setNumber(+Number - 1)}>-1</button>
       </div>
     </div>
   );
